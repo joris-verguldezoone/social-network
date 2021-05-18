c3e79a63093af719c25adbbda465c2b34d565a71
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-05-18 16:40:13 +0200

    tej node

--- js/nodejs.js
+++ js/nodejs.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+
+const httpServer = require("http").createServer();
 const { Server } = require("socket.io");
-const io = new Server(server);
-var cookieParser = require('cookie-parser')
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+// var cookieParser = require('cookie-parser')
+
+console.log(__dirname);
 
 var mysql = require('mysql');
-
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -19,58 +23,115 @@ con.connect(function (err) {
     console.log("Connected! at social-network");
 });
 
-function inscription()
-
-
-
-
-
-
-app.get('/inscription', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-    user_connected_list.push(socket.id);
-
-    for (i = 0; i < user_connected_list.length; i++) {
-        console.log(user_connected_list[i] + "coucou user_id")
-    }
-
-});
-
-server.listen(3000, () => {
-    console.log('listening on *:3000');
-});
-
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
 user_connected_list = [];
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
+    console.log('a user connected');
+    user_connected_list.push(socket.id);
 
-        console.log('message: ' + msg);
-        socket.broadcast.to(user_connected_list.toid).emit('sendMsg', { msg: data.msg, name: data.name });
-    });
+    for (i = 0; i < user_connected_list.length; i++) {
+        console.log(user_connected_list[i] + " user_" + i)
+        io.emit('connect_user', user_connected_list[i] + " user_" + i);
+    }
+    // console.log(sessions[client.session])
 });
 
+
+httpServer.listen(3800, () => {
+    console.log('listening on *:3800');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     }); // emit on console
 });
+// io.on('connection', (socket) => {
+//     socket.broadcast.emit('hi');
+//     socket.user_connected_list[0].emit()
 
+// });
 
+var GoogleAuth;
+var SCOPE = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+function handleClientLoad() {
+    // Load the API's client and auth2 modules.
+    // Call the initClient function after the modules load.
+    gapi.load('client:auth2', initClient);
+}
 
-/*
-instancier des tokens dans les cookies
-instancier des signatures dans les cookies
-recuperer le dom des cookies,
-faire les verification avec les decode des différents champs des cookies
+function initClient() {
+    // In practice, your app can retrieve one or more discovery documents.
+    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
 
-creer des doonées de navigation avec les data des user + des sockets
+    // Initialize the gapi.client object, which app uses to make API requests.
+    // Get API key and client ID from API Console.
+    // 'scope' field specifies space-delimited list of access scopes.
+    gapi.client.init({
+        'apiKey': 'cUZHuvtgvrP61Vzm_7gBVYwg',
+        'clientId': '377544883210-78q7fam3tcvvmi0q918soegdsiic24ea.apps.googleusercontent.com',
+        'discoveryDocs': [discoveryUrl],
+        'scope': SCOPE
+    }).then(function () {
+        GoogleAuth = gapi.auth2.getAuthInstance();
 
+        // Listen for sign-in state changes.
+        GoogleAuth.isSignedIn.listen(updateSigninStatus);
 
-*/
+        // Handle initial sign-in state. (Determine if user is already signed in.)
+        var user = GoogleAuth.currentUser.get();
+        setSigninStatus();
+
+        // Call handleAuthClick function when user clicks on
+        //      "Sign In/Authorize" button.
+        $('#sign-in-or-out-button').click(function () {
+            handleAuthClick();
+        });
+        $('#revoke-access-button').click(function () {
+            revokeAccess();
+        });
+    });
+}
+
+function handleAuthClick() {
+    if (GoogleAuth.isSignedIn.get()) {
+        // User is authorized and has clicked "Sign out" button.
+        GoogleAuth.signOut();
+    } else {
+        // User is not signed in. Start Google auth flow.
+        GoogleAuth.signIn();
+    }
+}
+
+function revokeAccess() {
+    GoogleAuth.disconnect();
+}
+
+function setSigninStatus() {
+    var user = GoogleAuth.currentUser.get();
+    var isAuthorized = user.hasGrantedScopes(SCOPE);
+    if (isAuthorized) {
+        $('#sign-in-or-out-button').html('Sign out');
+        $('#revoke-access-button').css('display', 'inline-block');
+        $('#auth-status').html('You are currently signed in and have granted ' +
+            'access to this app.');
+    } else {
+        $('#sign-in-or-out-button').html('Sign In/Authorize');
+        $('#revoke-access-button').css('display', 'none');
+        $('#auth-status').html('You have not authorized this app or you are ' +
+            'signed out.');
+    }
+}
+
+function updateSigninStatus() {
+    setSigninStatus();
+}
