b584ce1314b22448bcf8b6e0fb962ba516c277b5
William² <william.kies@laplateforme.io>
2021-05-21 15:03:25 +0200

    router en cours, accès aux pages, accueil - profil - chat

--- index.php
+++ index.php
@@ -10,19 +10,15 @@ session_start();
 spl_autoload_register(function ($className)
 {
     $className = str_replace('App', 'src', $className);
-    $filePath =  str_replace('\\', '/', $className) . '.php'; // Peut etre remplacer le .php en .twig ???
+    $filePath =  str_replace('\\', '/', $className) . '.php';
     if (file_exists($filePath)) { require($filePath); }
 });
-$app = AppFactory::create();
 
+$app = AppFactory::create();
 
 define('BASE_PATH', rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/'));
 define('HTTP_HOST', $_SERVER["HTTP_HOST"]);
 $app->setBasePath(BASE_PATH);
-echo __DIR__;
-var_dump(BASE_PATH);
-var_dump($_SERVER["SCRIPT_NAME"]);
-var_dump($_SERVER["HTTP_HOST"]);
 
 $app -> addRoutingMiddleware();
 
@@ -30,12 +26,8 @@ $errorMiddleware = $app -> addErrorMiddleware(true, true, true);
 
 $app -> get('/', ManageController::class . ':main');
 
-$app -> map(['GET', 'POST'],'/inscription', ManageController::class . ':inscription');
-
-$app -> map(['GET', 'POST'], '/connection', ManageController::class . ':connection');
-
-$app -> get('/log-out', ManageController::class . ':logOut');
-
 $app -> map(['GET', 'POST'], '/profil', ManageController::class . ':profil');
+
+$app -> get('/chat', ManageController::class . ':chat');
 
 $app -> run();
--- src/Controller/ManageController.php
+++ src/Controller/ManageController.php
@@ -22,141 +22,17 @@ class ManageController extends Controller
         return $response;
     }
 
-    /**
-     * Vérifie, charge et affiche inscription.twig
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param $args
-     * @return Response
-     */
-    public function inscription(Request $request, Response $response, $args)
-    {
-        $method = $request -> getMethod();
-        if ($method == 'POST')
-        {
-            $params = (array)$request -> getParsedBody();
-
-            $login = $params['createLogin'];
-            $email = $params['createEmail'];
-            $password = $params['createPassword'];
-            $confirm_password = $params['confirmCreatePassword'];
-
-            $InscriptionController = new \App\controller\InscriptionController();
-            $InscriptionController -> register($login, $email, $password, $confirm_password);
-        } else {
-            $login = '';
-            $email = '';
-            $password = '';
-            $confirm_password = '';
-        }
-
-        $this -> preloadTwig();
-        $response -> getBody() -> write($this -> twig -> render(
-            'inscription.twig',
-            [
-                'BASE_PATH' => BASE_PATH,
-                "HTTP_HOST" => HTTP_HOST,
-                'method' => $method,
-                'login' => $login,
-                'email' => $email,
-                'password' => $password,
-                'confirm_password' => $confirm_password
-            ]
-        ));
-        return $response;
-    }
-
-    /**
-     * Vérifie, charge et affiche connection.twig
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param $args
-     * @return Response
-     */
-    public function connection(Request $request, Response $response, $args)
-    {
-        $method = $request -> getMethod();
-        if ($method == 'POST')
-        {
-            $params = (array)$request -> getParsedBody();
-
-            $login = $params['logLogin'];
-            $password = $params['logPassword'];
-
-            $connectionController = new ConnectionController();
-            $connectionController -> connect($login, $password);
-        } else {
-            $login = '';
-            $password = '';
-        }
-
-        $this -> preloadTwig();
-        $response -> getBody() -> write($this -> twig -> render(
-            'connection.twig',
-            [
-                'BASE_PATH' => BASE_PATH,
-                "HTTP_HOST" => HTTP_HOST,
-                'method' => $method,
-                'login' => $login,
-                'password' => $password
-            ]
-        ));
-
-        return $response;
-    }
-
-    /**
-     * Permet de déconnecter l'utilisateur lorsque que celui-ci intéragis avec le bouton déconnection
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param $args
-     * @return Response
-     */
-    public function logOut(Request $request, Response $response, $args)
-    {
-        session_destroy();
-        return $response
-            -> withHeader('Location', 'https://' . HTTP_HOST . BASE_PATH)
-            -> withStatus(302);
-    }
-
     public function profil(Request $request, Response $response, $args)
     {
-        $method = $request -> getMethod();
-        if ($method == 'POST')
-        {
-            $params = (array)$request -> getParsedBody();
-
-            $login = $params['profilLogin'];
-            $email = $params['profilEmail'];
-            $password = $params['profilPassword'];
-            $confirm_password = $params['confirmProfilPassword'];
-
-            $controllerProfil = new ProfilController();
-            $controllerProfil -> modifyProfil($login, $email, $password, $confirm_password);
-        } else {
-            $login = '';
-            $email = '';
-            $password = '';
-            $confirm_password = '';
-        }
-
         $this -> preloadTwig();
-        $response -> getBody() -> write($this -> twig -> render(
-            'profil.twig',
-            [
-                'BASE_PATH' => BASE_PATH,
-                'HTTP_HOST' => HTTP_HOST,
-                'method' => $method,
-                'login' => $login,
-                'password' => $password,
-                'email' => $email
-            ]
-        ));
+        $response -> getBody() -> write($this -> twig -> render('profil.twig'));
+        return $response;
+    }
 
+    public function chat(Request $request, Response $response, $args)
+    {
+        $this -> preloadTwig();
+        $response -> getBody() -> write($this -> twig -> render('chat.twig'));
         return $response;
     }
 }
