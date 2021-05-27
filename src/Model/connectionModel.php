76d274c20c728f16f0984df3b5b95e21a28ed150
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-05-27 10:18:10 +0200

    user_google

---
+++ src/Model/connectionModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+class ConnectionModel extends Model
+{
+
+    public function registerGoogleUser($id_google, $login, $image)
+    { // ce sera l'image en mignature 
+
+        $sql = "INSERT INTO user_log (id_google, login, image) VALUES (:id_google, :login, :image)";
+        $result = $this->pdo->prepare($sql);
+        $result->bindValue(':id_google', $id_google, \PDO::PARAM_STR);
+        $result->bindValue(':login', $login, \PDO::PARAM_STR);
+        $result->bindValue(':image', $image, \PDO::PARAM_STR);
+        $result->execute();
+    }
+}
