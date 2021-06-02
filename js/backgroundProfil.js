01e94c6fa133d34b1026fb5f4994ee4d8c091425
William² <william.kies@laplateforme.io>
2021-06-02 15:32:52 +0200

    Faire le JS pour changer l'image de couverture

---
+++ js/backgroundProfil.js
@@ -0,0 +1,11 @@
+var btn = document.getElementsById('#submitBackground');
+
+/* A Faire :
+*
+* Faire une fonction qui permet de changer l'image de couverture
+* du profil de l'utilisateur.
+*
+* Si il se déconnecte et reconnecte, l'image est la même qu'a sa
+* deconnexion.
+*
+*  */
