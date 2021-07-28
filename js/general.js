0cbf1e8142380d4c01aa1fec1d72a0a1ca5730d1
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-07-28 11:49:07 +0200

    essaie hover js

--- js/general.js
+++ js/general.js
@@ -1,6 +1,25 @@
-function displayNotif() {
+// function displayNotif() {
+//     if (displayNotif.style.display === "none") {
+//     } else {
+//     }
+// }
+$(document).ready(function () {
+    // let displayNotif = document.getElementById('displayNotif')
+    let displayNotig = $('#displayNotif')
+    $(document).mousemove(function () {
+        if ($("#displayNotif:hover").length != 0) {
+            $('#displayNotif').show()
+            console.log('show')
+        } else {
+            $('#displayNotif').hide();
+            console.log('hide')
+        }
+    });
+});
 
-}
+// $("#id").css("display", "none");
+// $("#id").css("display", "block");
+
 function displayPostIndex() {
     $.$.ajax({
         type: "POST",
